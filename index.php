86b53ffce1bc14c95ee7ffbe4b4f4c3f3cf4bafe
Luis Batista <luisbatista@domdigital.com>
2011-04-11 18:53:18 +0100

    Versao 2.0

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <?php
-function getVersion(){ return "1.0"; }
+function getVersion(){ return "2.0"; }
 function ShowIndexMessage($msgindex){ echo "<strong>$msgindex".getVersion()."</strong><br >"; }
 ShowIndexMessage("Estamos no Index.php - ");
 echo "<a href=\"msg.php\">Ir para msg.php</a>";
